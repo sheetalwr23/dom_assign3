c14d61eeadd624ab9978a1e036d86a14649a84de
Sheetal <Sheetalwr23@gmail.com>
2023-03-20 16:50:41 +0530

    all task 2 and 3  are completed

--- dom.js
+++ dom.js
@@ -1,13 +1,13 @@
-// task1
+// task2
 var headtit = document.getElementById("header-title");
 headtit.innerText = "hello";
 headtit.textContent = "hello content";
 headtit.style.border = "2px solid black";
-// task2
+// task3[i]
 var header = document.getElementById("main-header");
 header.style.borderBottom = "4px solid black";
 
-// task3
+// task3[ii]
 var list = document.getElementsByClassName("list-group-item");
 console.log(list);
 // list[0].style.fontWeight = "bold";
