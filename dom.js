23d14db2111f4cd6dd6d5ea9141e9e22597256ed
Sheetal <Sheetalwr23@gmail.com>
2023-03-21 13:41:40 +0530

    querry selector assignment 6 is done

--- dom.js
+++ dom.js
@@ -1,5 +1,33 @@
-//~~~~~~~~~~~getElementByTagName
-var li = document.getElementsByTagName("li");
-console.log(li[1]);
-li[1].textContent = "hello2";
-li[1].style.fontWeight = "bold";
+//~~~~~~querrySelector
+var input = document.querySelector("input");
+// console.log(input);
+input.value = "hello world";
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "send";
+// submit.style.backgroundColor = "pink";
+//~~~~~~~child member
+// var lastItem = document.querySelector(".list-group-item:last-child");
+// lastItem.style.color = "blue";
+
+//~~~~Css pseudo selectors
+// var secondItem = document.querySelector(".list-group-item:nth-child(1)");
+// secondItem.style.backgroundColor = "skyblue";
+
+//~~~~~~~~~~assignment 30 to 37min
+var li = document.getElementsByClassName("list-group-item");
+li[1].style.backgroundColor = "green";
+li[2].style.visibility = "hidden";
+
+//~~~~~~~~querrySelectorAll
+//~~~~assignmnet 37 to end
+// var titles = document.querySelectorAll(".title");
+// titles[0].innerText = "my items";
+
+// var odd = document.querySelectorAll("li:nth-child(odd)");
+// var even = document.querySelectorAll("li:nth-child(even)");
+
+// for (let i = 0; i < odd.length; i++) {
+//   odd[i].style.backgroundColor = "green";
+//   even[i].style.backgroundColor = "blue";
+// }
