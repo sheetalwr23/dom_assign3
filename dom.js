f14d032155015a69bc0e9a0465dee74f4e1e51d6
Sheetal <Sheetalwr23@gmail.com>
2023-03-21 12:35:42 +0530

    assignment 5 getElementsByTagName

--- dom.js
+++ dom.js
@@ -1,20 +1,5 @@
-// task2
-var headtit = document.getElementById("header-title");
-headtit.innerText = "hello";
-headtit.textContent = "hello content";
-headtit.style.border = "2px solid black";
-// task3[i]
-var header = document.getElementById("main-header");
-header.style.borderBottom = "4px solid black";
-
-// task3[ii]
-var list = document.getElementsByClassName("list-group-item");
-console.log(list);
-// list[0].style.fontWeight = "bold";
-
-list[3].innerHTML = "hello";
-list[2].style.backgroundColor = "green";
-
-for (let i = 0; i < list.length; i++) {
-  list[i].style.fontWeight = "bold";
-}
+//~~~~~~~~~~~getElementByTagName
+var li = document.getElementsByTagName("li");
+console.log(li[1]);
+li[1].textContent = "hello2";
+li[1].style.fontWeight = "bold";
